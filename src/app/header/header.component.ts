15e7a16715dc4760c05efc0a6f1e1f1ffaf9ec35
IlmarKerner <kernerilmar@gmail.com>
2023-02-18 14:36:56 +0100

    todo component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,26 +1,92 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   currentTime: Date = new Date();
   currentDay: Date = new Date();
-
+  @ViewChild('todoContainer')
+  todoContainer!: ElementRef;
 
   ngOnInit(): void {
     setInterval(() => {
       this.getTime();
+      this.getDate();
     }, 1000);
   }
 
   constructor() {}
+
+  getSeconds() {
+    return ('0' + this.currentTime.getSeconds()).slice(-2); // 05
+  }
+
+  getMinutes() {
+    return ('0' + this.currentTime.getMinutes()).slice(-2);
+  }
+
+  getHours() {
+    return ('0' + this.currentTime.getHours()).slice(-2);
+  }
+
+  getDate() {
+    return ('0' + this.currentDay.getDate()).slice(-2);
+  }
+
+  getMonth() {
+    switch (this.currentDay.getMonth()) {
+      case 0:
+        return 'January';
+      case 1:
+        return 'February';
+      case 2:
+        return 'March';
+      case 3:
+        return 'April';
+      case 4:
+        return 'May';
+      case 5:
+        return 'June';
+      case 6:
+        return 'July';
+      case 7:
+        return 'August';
+      case 8:
+        return 'September';
+      case 9:
+        return 'October';
+      case 10:
+        return 'November';
+      case 11:
+        return 'December';
+      default:
+        return '';
+    }
+  }
+
+  getFullYear() {
+    return this.currentDay.getFullYear();
+  }
 
   getTime() {
     this.currentTime.setSeconds(new Date().getSeconds());
     this.currentTime.setMinutes(new Date().getMinutes());
     this.currentTime.setHours(new Date().getHours());
   }
+
+  date() {
+    this.currentDay.getDate();
+    this.currentDay.getMonth();
+    this.currentDay.getFullYear();
+  }
+
+  
+  saveTodo() {
+
+  }
 }
+
+
--- src/app/todo-container/todo-container.component.ts
+++ src/app/todo-container/todo-container.component.ts
@@ -1,22 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore, collectionData, collection, setDoc, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todo-container',
   templateUrl: './todo-container.component.html',
-  styleUrls: ['./todo-container.component.scss']
+  styleUrls: ['./todo-container.component.scss'],
 })
-export class TodoContainerComponent{
+export class TodoContainerComponent {
   todo: string = '';
+  MyTodos$: Observable<any>;
+  MyTodos:Array<any>; // könnte man auch ausklammern
+  title:string = '';
+  description:string = '';
+
+  constructor(private firestore: Firestore) {
+    const coll = collection(firestore, 'MyTodos');
+    this.MyTodos$ = collectionData(coll);
+
+    this.MyTodos$.subscribe( (newTodos) => {
+      console.log('neue todos', newTodos)
+      // alert('neuer Eintrag!') beispielhaft zusätzlich hinzugefügt
+      this.MyTodos = newTodos;
+    });
+  }
+
+
+  ngOnInit(): void {}
+
+  openEnterfield() {
+    document.getElementById('fill-card')?.classList.remove('d-none');
+    document.getElementById('fill-card')?.classList.add('show');
+    document.getElementById('fill-card')?.classList.remove('gone');
+  }
+
+  saveTodo() {
+    const coll = collection(this.firestore, 'MyTodos');
+    setDoc(doc(coll), {title: this.title, description: this.title}); 
+    this.title = '';
+    this.description = '';
+    this.cancel();
+  }
   
-  ngOnInit(): void {
-    
-
-
-
+  cancel() {
+    document.getElementById('fill-card')?.classList.remove('show');
+    document.getElementById('fill-card')?.classList.add('gone');
+    setTimeout(() => {
+      document.getElementById('fill-card')?.classList.add('d-none');
+    }, 225);
   }
-
-  openDialog() {
-
-  }
-
 }
